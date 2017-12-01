18c0566413ebc92eaff17f94daab425161e639dc
Jimbles <j.avery.10@ucl.ac.uk>
2017-12-01 10:45:15 +0000

    Added Topic 8 examples

---
+++ Topic_8/q1_4.m
@@ -0,0 +1,102 @@
+%% question 1
+
+h=0.01; %step size
+N=60; %number of steps
+
+x=0:h:h*(N-1); %x values start at 0
+
+y=zeros(N,1); %initialise
+
+y(1)=1; %initial conditions y(x=0)=1
+
+%eulers iterations for equation y'=y
+for iStep=1:N-1
+    ydash(iStep)=y(iStep); % current value y'=y
+    y(iStep+1)=y(iStep)+h*ydash(iStep); % next value
+end
+
+%analytical solution
+yactual=exp(x);
+
+figure;
+hold all
+plot(x,y,'-x')
+plot(x,yactual);
+hold off
+xlabel('x')
+ylabel('y')
+legend('Euler','Actual')
+title(['Step size ' num2str(h)]);
+
+
+
+
+%% question 2
+
+h=0.01; %step size
+N=6; %number of steps
+
+x=0:h:h*(N-1); %x values start at 0
+
+y=zeros(N,1); %initialise
+
+y(1)=1; %initial conditions y(x=0)=1
+
+%eulers iterations for equation y'=y
+for iStep=1:N-1
+    ydash=y(iStep); % current value y'=y
+    y(iStep+1)=y(iStep)+h*ydash; % next value
+end
+
+%analytical solution
+yactual=exp(x);
+
+figure;
+hold all
+plot(x,y,'-x')
+plot(x,yactual);
+hold off
+
+
+%% question 3
+
+h=0.1; %step size
+N=6; %number of steps
+
+x=0:h:h*(N-1); %x values start at 0
+
+y=zeros(N,1); %initialise
+
+y(1)=0; %initial conditions y(x=0)=0
+
+%eulers iterations for equation y'=(y-x)^2
+for iStep=1:N-1
+    ydash=(y(iStep)-x(iStep))^2; % current value y'=(y-x)^2
+    y(iStep+1)=y(iStep)+h*ydash; % next value
+end
+
+%% question 4
+
+h=0.1; %step size
+N=6; %number of steps
+
+x=0:h:h*(N-1); %x values start at 0
+
+y=zeros(N,1); %initialise
+ydash=y;
+hydash=y;
+
+y(1)=0; %initial conditions y(x=0)=0
+
+%eulers iterations for equation y'=(y+x)^2
+for iStep=1:N-1
+    ydash(iStep)=(y(iStep)+x(iStep))^2; % current value y'=(y+x)^2
+    hydash(iStep)=+h*ydash(iStep); 
+    y(iStep+1)=y(iStep)+ hydash(iStep);% next value
+end
+
+figure;
+hold all
+plot(x,y,'-x')
+% plot(x,yactual);
+hold off
---
+++ Topic_8/q5_8.m
@@ -0,0 +1,82 @@
+%% question 5
+
+h=0.1; %step size
+N=6; %number of steps
+
+x=0:h:h*(N-1); %x values start at 0
+
+y=zeros(N,1); %initialise
+
+y(1)=1; %initial conditions y(x=0)=1
+
+%modifyed eulers iterations for equation y'=y
+for iStep=1:N-1
+    ydashest1=y(iStep); % current value y'=y
+    yest=y(iStep)+h*ydashest1; %estimate value first
+    ydashest2=yest; %y' using yest y'=yest
+    y(iStep+1)=y(iStep)+0.5*h*(ydashest1+ydashest2); % next value taking average of both estimates
+end
+
+yactual=exp(x);
+
+figure;
+hold all
+plot(x,y,'-x')
+plot(x,yactual);
+hold off
+
+
+%% question 6
+
+h=0.1; %step size
+N=6; %number of steps
+
+x=0:h:h*(N-1); %x values start at 0
+
+y=zeros(N,1); %initialise
+
+y(1)=0.2; %initial conditions y(x=0)=.2
+
+%modifyed eulers iterations for equation y'=y-y^2
+for iStep=1:N-1
+    ydashest1=y(iStep)-y(iStep)^2; % current value y'=y-y^2
+    yest=y(iStep)+h*ydashest1; %estimate value first
+    ydashest2=yest-yest^2; %y' using yest y'=yest-yest^2
+    y(iStep+1)=y(iStep)+0.5*h*(ydashest1+ydashest2); % next value taking average of both estimates
+end
+%% question 7
+
+h=0.1; %step size
+N=6; %number of steps
+
+x=0:h:h*(N-1); %x values start at 0
+
+y=zeros(N,1); %initialise
+
+y(1)=1; %initial conditions y(x=0)=1
+
+%modifyed eulers iterations for equation y'=xy^2
+for iStep=1:N-1
+    ydashest1=x(iStep)*(y(iStep)^2); % current value y'=xy^2
+    yest=y(iStep)+h*ydashest1; %estimate value first
+    ydashest2=x(iStep+1)*(yest^2); %y' using yest y'=xyest^2 NOTE X(n+1)!!!!
+    y(iStep+1)=y(iStep)+0.5*h*(ydashest1+ydashest2); % next value taking average of both estimates
+end
+%% question 8
+
+h=0.1; %step size
+N=6; %number of steps
+
+x=0:h:h*(N-1); %x values start at 0
+
+y=zeros(N,1); %initialise
+
+y(1)=0; %initial conditions y(x=0)=0
+
+%modifyed eulers iterations for equation y'=1-0.25y^2
+for iStep=1:N-1
+    ydashest1=1-0.25*(y(iStep)^2); % current value y'=xy^2
+    yest=y(iStep)+h*ydashest1; %estimate value first
+    ydashest2=1-0.25*(yest^2); %y' using yest y'=xyest^2 NOTE X(n+1)!!!!
+    y(iStep+1)=y(iStep)+0.5*h*(ydashest1+ydashest2); % next value taking average of both estimates
+end
